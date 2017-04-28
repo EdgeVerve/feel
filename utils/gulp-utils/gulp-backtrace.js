e50cac1ac875c83a4e90dcf0cb48328585dbe124
pragyandas <pragyandas@msn.com>
2017-04-28 19:06:50 +0530

    Changed license information in all files

--- utils/gulp-utils/gulp-backtrace.js
+++ utils/gulp-utils/gulp-backtrace.js
@@ -1,8 +1,8 @@
-/*
-�2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary), Bangalore, India. All Rights Reserved.
-The EdgeVerve proprietary software program ("Program"), is protected by copyrights laws, international treaties and other pending or existing intellectual property rights in India, the United States and other countries.
-The Program may contain/reference third party or open source components, the rights to which continue to remain with the applicable third party licensors or the open source community as the case may be and nothing here transfers the rights to the third party and open source components, except as expressly permitted.
-Any unauthorized reproduction, storage, transmission in any form or by any means (including without limitation to electronic, mechanical, printing, photocopying, recording or  otherwise), or any distribution of this Program, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law.
+/*  
+*  
+*  ©2016 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),  
+*  Bangalore, India. All Rights Reserved.  
+*   
 */
 const Tracer = require('pegjs-backtrace');
 const Parser = require('../../trace/feel');
