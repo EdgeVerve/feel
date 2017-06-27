6d1c311b017abe77b9a00ba132011bd7562e890f
pragyandas <pragyandas@msn.com>
2017-06-27 12:18:16 +0530

    added comparision test case using date time

--- test/comparision-expression/feel-comparision-expression.build.spec.js
+++ test/comparision-expression/feel-comparision-expression.build.spec.js
@@ -1,8 +1,8 @@
-/*  
-*  
-*  ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),  
-*  Bangalore, India. All Rights Reserved.  
-*   
+/*
+*
+*  ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+*  Bangalore, India. All Rights Reserved.
+*
 */
 var chalk = require('chalk');
 var chai = require('chai');
@@ -88,6 +88,24 @@ describe(chalk.blue('Comparision expression ast parsing test'), function() {
         var parsedGrammar = FEEL.parse(text);
         parsedGrammar.build(_context).then(result => {
             expect(result).not.to.be.undefined;
+            done();
+        }).catch(err => done(err));
+    });
+
+    it('Successfully compare dates successfully', function(done) {
+        var text = 'date("2012-12-25") > date("2012-12-24")';
+        var parsedGrammar = FEEL.parse(text);
+        parsedGrammar.build().then(result => {
+            expect(result).to.be.true;
+            done();
+        }).catch(err => done(err));
+    });
+
+    it('Successfully compare dateandtime successfully', function(done) {
+        var text = 'dateandtime("2012-12-24T23:59:00") < dateandtime("2012-12-25T00:00:00")';
+        var parsedGrammar = FEEL.parse(text);
+        parsedGrammar.build().then(result => {
+            expect(result).to.be.true;
             done();
         }).catch(err => done(err));
     });
