998c29b9d09567fc5f19ec7f7daa3acbf4ac7dd2
sambit <Sambit_patra@edgeverve.com>
2017-07-10 13:28:36 +0530

    added loopback belongsTo and hasOne relation support in FEEL decision table

---
+++ utils/built-in-functions/loopback-relation-functions/relation-functions.js
@@ -0,0 +1,42 @@
+const FEEL = require('../../../dist/feel');
+
+const preparePayload = (modelName, data) => new Promise((resolve, reject) => {
+  const context = '{ fetchPayload : function() external { js : {dependencies : [{ loopback : "loopback"}], signature : "const Model = loopback.getModel(modelName);const payload = new Model(data);done(null, payload)"}}}';
+  const text = 'fetchPayload()';
+  const parsedContext = FEEL.parse(context);
+  const parsedText = FEEL.parse(text);
+  const payload = {};
+
+  payload.modelName = modelName;
+  payload.data = data;
+  parsedContext.build(payload).then(ctx => parsedText.build(Object.assign({}, ctx, payload))).then((result) => {
+    resolve(result);
+  }).catch((err) => {
+    reject(err);
+  });
+});
+
+const relationCb = (resolve, reject) => (err, res) => {
+  if (err) {
+    reject(err);
+  } else {
+    resolve(res);
+  }
+};
+
+const lbRelation = (fnStr, data) => {
+  const fnSeries = fnStr.split('.');
+  const fnArray = fnSeries.slice(1, fnSeries.length - 1).map(str => str.substring(0, str.length - 2));
+  const options = data.options;
+
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-underscore-dangle
+    preparePayload(options.modelName, data.__data).then(payload => fnArray.reduce((promise, fn) => promise.then(parent => new Promise((resolve, reject) => {
+      parent[fn](options, relationCb(resolve, reject));
+    })), Promise.resolve(payload))).then((res) => {
+      resolve(res);
+    }).catch(err => reject(err));
+  });
+};
+
+module.exports = { lbRelation };
