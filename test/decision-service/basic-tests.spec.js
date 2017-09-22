833746b8648ed759633bedc38d7e599cc617f1e8
Anukriti <anukriti_vijay@edgeverve.com>
2017-09-22 13:48:12 +0530

    Added copyright notice for oe-cloud 1.0.0 release

--- test/decision-service/basic-tests.spec.js
+++ test/decision-service/basic-tests.spec.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ *
+ */
 // const XLSX = require('xlsx');
 const chai = require('chai');
 // process.on('error', (e) => console.error(e))
